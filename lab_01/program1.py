dd6be7e5d074ee38d1aa7b92e7a67aa09b9e819d
Shulamith <shulamit.dashevsky@gmail.com>
2018-09-06 09:10:21 -0400

    cigar party

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -7,3 +7,11 @@ print(greeter("Stan"))
 print(repeat_string("hello"))
 def make_abba(a, b):
   return a+b+b+a
+def cigar_party(cigars, is_weekend):
+  if is_weekend and cigars >= 40:
+    return True
+  else:
+    if 40<=cigars<=60:
+     return True
+    else:
+     return Fal
