d5ddbfb5e03a7c3f11a023671a836edd78a3bc75
Shulamith <shulamit.dashevsky@gmail.com>
2018-09-06 08:56:32 -0400

    testing

---
+++ lab_01/program1.py
@@ -0,0 +1,9 @@
+def greeter(name):
+    return "Hello "+name+"!"
+def repeat_string(word):
+    return(word+word)
+print(greeter("Ollie"))
+print(greeter("Stan"))
+print(repeat_string("hello"))
+def make_abba(a, b):
+  return a+b+b+a
