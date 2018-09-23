76e90a119afa7d8cfc4895b559ff2fde934a6ca3
Shulamith <shulamit.dashevsky@gmail.com>
2018-09-22 22:06:28 -0400

    FizzBuzz

---
+++ lab-02/FizzBuzz.py
@@ -0,0 +1,16 @@
+
+def fizzBuzz(number):
+    x=1
+    while x<=number:
+        if x % 15 == 0:
+            print("\nFizzBuzz")
+        elif x % 5 ==0:
+            print("Buzz")
+        elif x % 3 ==0:
+            print ("Fizz")
+        else:
+            print(x)
+        x=x+1
+        
+        
+print(fizzBuzz(100))
