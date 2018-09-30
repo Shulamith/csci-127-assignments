02936342ea33c716c74a07cc2283a7477591772a
Shulamith <shulamit.dashevsky@gmail.com>
2018-09-30 16:47:21 -0400

    Update squareroot.py

--- hw03/squareroot.py
+++ hw03/squareroot.py
@@ -11,12 +11,18 @@
 def mySquareroot(number):
     guess = 5
     i = 1
-    while ((guess * guess) != number and i<20):
+##    oldguess = 0
+    print("This is my first guess:")
+    while ((guess * guess) != number and i<=20): #either if perfect square or if python continues to repeat the same guess
+##        oldguess = guess
         print(guess)
         guess = ((guess + (number/guess))/2)
         i +=1
+    guess = str(round(guess, 2))
+    print(guess) 
+    print("The above is either the answer or the closest we can to this function")
         
-   if (i=20):
-       print ("There is no ration sqrt. It is approximatley...")
+##   if (i=20):
+##       print ("There is no ration sqrt. It is approximatley...")
 print(mySquareroot(16))
-print(mySquareroot(19))
+print(mySquareroot(1003))
