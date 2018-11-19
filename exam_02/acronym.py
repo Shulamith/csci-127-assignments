945af44ab7c361b50f3d5e6ebd86c20bbbee5e4c
Shulamith <shulamit.dashevsky@gmail.com>
2018-11-19 09:23:34 -0500

    exam

--- exam_02/acronym.py
+++ exam_02/acronym.py
@@ -6,8 +6,15 @@ def makeacronym(w):
         i+=1
         if character == ' ':
             acronym.append(w[i])
-##    acronymW = acronym.join(" ")
-    return acronym
+    acronymW = "".join(acronym)
+    return acronymW
             
 
-print(makeacronym("hello my name is"))
+print("hello my name is:",makeacronym("hello my name is"))
+print("See ya:",makeacronym("See ya"))
+print("Laughing out loud:",makeacronym("Laughing out loud"))
+print("What Do You Mean:",makeacronym("What Do You Mean"))
+print("in my humble opinion:",makeacronym("in my humble opinion"))
+print("don't worry:",makeacronym("don't worry"))
+print("no problem:",makeacronym("no problem"))
+print("No:",makeacronym("No"))
--- exam_02/scrabble.py
+++ exam_02/scrabble.py
@@ -5,10 +5,26 @@ tupleFour='F','H','V','W','Y'
 tupleFive='K'
 tupleEight='J','X'
 tupleTen='Q','Z'
-d = {tupleOne:1,tupleTwo:2,tupleThree:3,tupleFour:4,tupleFive:5,tupleEight:8,tupleTen:10}
+##d = {tupleOne:1,tupleTwo:2,tupleThree:3,tupleFour:4,tupleFive:5,tupleEight:8,tupleTen:10}
+##def score(w):
+##    points=0
+##    for letter in w:
+##        point = d.get(letter) 
+##        print (point)
+##        
+##print(score("HELLO"))
+
+d = {'A':1,'E':1,'I':1,'O':1,'U':1,'L':1,'N':1,'R':1,'S':1,'T':1,'D':2,'G':2,'B':3,'C':3,'M':3,'P':3,'F':4,'H':4,'V':4,'W':4,'Y':4,'K':5,'J':8,'X':8,'Q':10,'Z':10}
 def score(w):
+    points=0
     for letter in w:
-        d[letter] ==point
-        print(point)
+        point = d.get(letter) 
+        points=points+point
+    return points
         
-print(score("HELLO"))
+print("HELLO",score("HELLO"))
+print("A",score("A"))
+print("ENCYCLOPEDIA",score("ENCYCLOPEDIA"))
+print("ZEBRA",score("ZEBRA"))
+print("QUEUE",score("QUEUE"))
+print("JUG",score("JUG"))
