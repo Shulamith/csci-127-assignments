6c32bf752912d184ec06f1eae128d55b0060a69b
Shulamith <shulamit.dashevsky@gmail.com>
2018-11-26 09:23:52 -0500

    run

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,21 +1,58 @@
+##def encode(s):
+##    l=[]  
+##    for i in range(0,len(s)-1):
+##        count = 1
+##        x = True
+##        y=1
+##        while x is True and y < len(s)-1:
+##            if s[i]==s[i+y]:
+##                count+=1
+##                y+=1
+##            else:
+##                x = False       
+##        l.append([s[i],count])
+##    return l
+##        
+##def encode(s):
+##    l=[]  
+##    for i in range(0,len(s)-1):
+##        y=1
+##        count=1
+##        if s[i]==s[i+y]:
+##            count+=1
+##            y+=1      
+##        l.append([s[i],count])
+##    return l
+
 def encode(s):
-    l=[]  
+    l=[]
+    x=1
+    count=1
     for i in range(0,len(s)-1):
-        count = 1
-        x = True
-        y=1
-        while x is True and y < len(s)-1:
-            if s[i]==s[i+y]:
+        if i == len(s)-1:
+            l.append([s[i],count])
+            return l
+        while i+x < len(s)-1 and s[i]==s[i+x]:
                 count+=1
-                y+=1
-            else:
-                x = False
-                l.append([s[i],count])
-    return l
+                x+=1
+        l.append([s[i],count])
+    return l        
         
-    
+        
 print("abb",encode("abb"))
+##example = encode("abb")
 print("abcd",encode("abcd"))
+print("abccccccdd",encode("abccccccdd"))
 
 def decode(l):
-    
+    word=[]
+    for list in l:
+        letter = list[0]
+        times=list[1]
+        i=0
+        while i < times:
+            word.append(letter)
+            i+=1
+    wordString="".join(word)
+    return wordString        
+##print(decode(example))
--- exam_02/scrabble.py
+++ exam_02/scrabble.py
@@ -1,10 +1,10 @@
-tupleOne='A','E','I','O','U','L','N','R','S','T'
-tupleTwo='D','G'
-tupleThree='B','C','M','P'
-tupleFour='F','H','V','W','Y'
-tupleFive='K'
-tupleEight='J','X'
-tupleTen='Q','Z'
+tupleOne=('A','E','I','O','U','L','N','R','S','T')
+tupleTwo=('D','G')
+tupleThree=('B','C','M','P')
+tupleFour=('F','H','V','W','Y')
+tupleFive=('K')
+tupleEight=('J','X')
+tupleTen=('Q','Z'
 ##d = {tupleOne:1,tupleTwo:2,tupleThree:3,tupleFour:4,tupleFive:5,tupleEight:8,tupleTen:10}
 ##def score(w):
 ##    points=0
