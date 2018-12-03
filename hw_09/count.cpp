99312c239ceea6c1d372748869fb604f8916a862
Shulamith <shulamit.dashevsky@gmail.com>
2018-12-03 06:10:38 -0500

    error w guess

---
+++ hw_09/count.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int i;
+	i = 0;
+	while (i< 20){
+		std::cout << i << "\n";
+		i+=1;
+	}
+	return 0;
+}
---
+++ hw_09/guess.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main()
+{
+	int i, guess,response;
+	/* code */
+	guess = 0;
+	std::cout << "Type an integer between 0 and 99 inclusive" << "\n";
+	std::cin << i;
+
+
+
+	while (response != 0) {
+		std:cout << "Did you pick" << guess << 
+		"? If yes type 0 if the number is higher type 1, if the number is lower type -1" << "\n";
+		std::cin << response;
+		guess = guess + response;
+	
+
+}
+	std::cout << "End of the game";
+
+	return 0;
+}
