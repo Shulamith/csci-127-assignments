e55bf4cbd325938a363ec489b6a74e9f50a18df1
Shulamith <shulamit.dashevsky@gmail.com>
2018-12-03 06:20:12 -0500

    error w guess

--- hw_09/guess.cpp
+++ hw_09/guess.cpp
@@ -3,15 +3,15 @@ int main()
 {
 	int i, guess,response;
 	/* code */
-	guess = 0;
 	std::cout << "Type an integer between 0 and 99 inclusive" << "\n";
 	std::cin << i;
 
 
-
+	
 	while (response != 0) {
-		std:cout << "Did you pick" << guess << 
-		"? If yes type 0 if the number is higher type 1, if the number is lower type -1" << "\n";
+		int response, guess;
+		guess = 0;
+		std::cout << "Did you pick" << guess << "? If yes type 0 if the number is higher type 1, if the number is lower type -1" << "\n";
 		std::cin << response;
 		guess = guess + response;
 	
