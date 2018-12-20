ed7dd57748d72c3d09ef9a8f84dbd6943b3003ea
Shulamith <shulamit.dashevsky@gmail.com>
2018-12-20 10:28:11 -0500

    first

---
+++ final_2/dict.py
@@ -0,0 +1,40 @@
+##d = {2:['Hello']}
+##d[2].append('retro')
+##print(d)
+
+
+
+d={}
+def addline(d,line):
+    lowercase = line.lower();
+    words = lowercase.split();
+    for word in words:
+        if word[0] in d:
+            d[word[0]].append(word);
+        else:
+            d[word[0]] = [word];
+    return d
+print(addline(d,"hello My Name iS Holmes"))
+addline(d,"For me to rap like a computer must be in my genes")
+addline(d, "I got a laptop in my back pocket")
+addline(d,"her hardest HUE to hold")
+addline(d,"her early leaf's a flower")
+addline(d,"Thank you, NEXT")
+print(d)
+
+
+def spellcheck(d,word):
+    lowercase= word.lower();
+    if lowercase[0] in d and lowercase in d[lowercase[0]]:
+        return True
+    else:
+        return False
+    
+print("RAp",spellcheck(d,"RAp"))
+print("Holmes",spellcheck(d,"Holmes"))
+print("asdlfjksdfl",spellcheck(d,"asdlfjsdlf"))
+print("um",spellcheck(d,"um"))
+##print("RAp",spellcheck(d,"RAp"))
+
+    
+    
--- final_2/scrabble.py
+++ final_2/scrabble.py
@@ -11,4 +11,21 @@ def canMakeWord(letters,word):
 
 print("ladilmy","daily",canMakeWord("ladilmy","daily"))
 print("hellost","ham",canMakeWord("hellost","ham"))
-print("orppgma","prorgram",canMakeWord("orppgma","prorgram"))
+print("orppgma","program",canMakeWord("orppgma","program"))
+
+def withWild(letters,word):
+    remaining_letters = [];
+    for letter in letters:
+        remaining_letters.append(letter);
+    for letter in word:
+        if letter not in remaining_letters and '?' not in remaining_letters:
+            return False
+        elif letter in remaining_letters:
+            remaining_letters.remove(letter)
+        else:
+            remaining_letters.remove('?')
+    return True
+
+print("orppgma?","program",withWild("orppgma?","program"))
+print("?????","hello",withWild("?????","hello"))
+print("qwert?","teas",withWild("qwert?","teas"))
